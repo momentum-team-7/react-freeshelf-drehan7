33a5f2fe501f1972ffe13e20d7c38f101fd5227a
Dawud Rehan <d.rehan7@gmail.com>
2021-03-20 18:13:51 -0400

    m/l info good and transition works; TODO aria stuff

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -5,10 +5,12 @@ import Book from './Book';
 
 function App() {
 
+    const [isActive, setisActive] = useState("");
+
     return (
         <div className="main-container">
             { bookData.map((book, i) => (
-                <Book key={i} book={book} />
+                <Book key={i} book={book} isActive={isActive} setisActive={setisActive} />
             ))}
         </div>
 
--- freeshelf/src/Book.js
+++ freeshelf/src/Book.js
@@ -2,31 +2,26 @@ import React, { useState } from 'react';
 import './App.css';
 
 function Book(props) {
-    const [isOpen, setIsOpen] = useState(false);
+    // const [isOpen, setIsOpen] = useState(false);
 
-    const toggleFunc = (e) => {
-        setIsOpen(!isOpen);
-        e.target.textContent = isOpen ? 'Less Information' : 'More Information';
-    }
+
 
     return (
         <div key={props.key} className="book-container">
             <div className="book-info">
-                <h2>{props.book.title}</h2>
+                <h2 className="book-title">{props.book.title}</h2>
                 <p>{props.book.author}</p>
                 <p>{props.book.shortDescription}</p>
-                <button className="more-info-button" onClick={toggleFunc}>More Information</button>
-                {isOpen && (
-                    <div className="more-book-info">
-                        <p>URl: {props.book.url}</p>
-                        <p>Publisher: {props.book.publisher}</p>
-                        <p>Publication Date: {props.book.publicationDate}</p>
-                        <p>Full Description: {props.book.detailedDescription}</p>
-                    </div>
-                )}
+                <button className="more-info-button" onClick={() => props.setisActive(props.isActive === props.book.author ? '' : props.book.author)}>{props.isActive === props.book.author ? 'Less Info' : 'More Info'}</button>
+                <div className={(props.isActive === props.book.author) ? 'more-book-info show' : 'more-book-info'}>
+                    <p>URL: <a target="_blank" href={props.book.url}>{props.book.url}</a></p>
+                    <p>Publisher: {props.book.publisher}</p>
+                    <p>Publication Date: {props.book.publicationDate}</p>
+                    <p>Full Description: <br />{props.book.detailedDescription}</p>
+                </div>
             </div>
             <div className="book-img">
-                <img src={props.book.coverImageUrl} alt={props.book.title}></img>
+                <img src={props.book.coverImageUrl} onerror="this.src={'https://www.freeiconspng.com/uploads/no-image-icon-23.jpg'}"></img>
             </div>
 
         </div>
