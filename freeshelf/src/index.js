a0a93288107e3f1bcd7bbaf5f4534672f842c68d
Dawud Rehan <d.rehan7@gmail.com>
2021-03-20 10:50:12 -0400

    condensed version rendering finally. need to figure out (more info) button and impl

--- freeshelf/src/index.js
+++ freeshelf/src/index.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-
+import { bookData } from './book-data';
 
 function App() {
+
   return (
-    <h1>Testing go fuck yaself</h1>
+    <div className="main-container">
+      { bookData.map((book, i) => (
+        <div key={i} className='book-container'>
+          <div className="book-info">
+            <h1>{book.title}</h1>
+            <p>{book.author}</p>
+            <p>{book.shortDescription}</p>
+          </div>
+          <div className='book-image'>
+            <img src={book.coverImageUrl} alt="img"></img>
+          </div>
+
+        </div>
+      ))}
+
+
+    </div>
+
   );
 }
 
